e4c8c11110ce1ecd4e9e7d5d441937b5017f0b48
siddy246 <sidm1_11@worc.ac.uk>
2014-02-18 15:42:04 +0000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,95 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+var filetext  = "";
+var fileEntry;
+var fileBinding;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
-};
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// device APIs are available
+function onDeviceReady() {
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+}
+
+//get access to file and CREATE if does not exists
+function gotFS(fileSystem) {
+ 	fileSystem.root.getFile("test.txt", {create: true, exclusive: false}, gotFileEntry, fail);
+}
+
+//get file entry
+function gotFileEntry(fileEntry) {
+	this.fileEntry = fileEntry;
+	fileEntry.file(gotFile, fail);
+}
+
+//get file itself
+function gotFile(file){
+	readAsText(file);
+}
+
+//READ text from file - assumes that the file contains 
+function readAsText(file) {
+	var reader = new FileReader();
+	
+	//this code is run one the file as been completely read
+	reader.onloadend = function(evt) {
+		
+		//store the new string in 'filetext'
+		filetext = evt.target.result;
+		
+		//update the binding 
+		fileBinding.set({
+			filetext: filetext
+		});
+    };
+	
+	//begin reading
+   	reader.readAsText(file);
+}
+
+
+//UDPATE file contents - called when submit button is pressed
+function writeFile()
+{
+	fileEntry.createWriter(
+		function (writer) { 
+			writer.write(filetext);
+		}, 
+		fail
+	);
+}
+
+//DELETE file
+function deleteFile()
+{
+	
+	fileEntry.remove(
+		function () {
+			alert("Deleted file");
+		}, 
+		fail
+	);
+	
+	//reload file system
+  	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+}
+
+function fail(error) {
+	alert("Cannot use file: " + error.code);
+}
+
+//
+//RactiveJS 	
+//
+
+//binding between variable 'filetext' and the template 
+var fileBinding = new Ractive({
+	el: 'container',
+	template: '#template',
+	data: { filetext: filetext}
+});
+
+
+//detects changes in the text box and updates the 'filetext' value with the new value
+fileBinding.observe( 'filetext', function ( newValue, oldValue ) {
+  filetext = newValue; 
+});
